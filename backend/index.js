2eec007f1c98a76e19896d308e65ab5a7652db54
Paul Wei <pwei1@ufl.edu>
2022-05-23 18:17:52 -0400

    different resolve path

--- backend/index.js
+++ backend/index.js
@@ -39,13 +39,13 @@ const path = require("path");
 
 //Access production build
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static(path.join(__dirname, '..', 'frontend/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '..', 'frontend/build/index.html'));
-    });
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    })
 }
 
-app.get("/", (req, res) => res.send("Hello, this is the server!"));
+// app.get("/", (req, res) => res.send("Hello, this is the server!"));
 
 require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
