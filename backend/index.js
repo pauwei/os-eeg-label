986a3d7b5b8779bc85249627c829a1a1f309be29
Paul Wei <pwei1@ufl.edu>
2022-05-23 12:59:54 -0400

    directory update

--- backend/index.js
+++ backend/index.js
@@ -39,9 +39,9 @@ const path = require("path");
 
 //Access production build
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('../frontend/build'));
+    app.use(express.static(path.join(__dirname, '..', 'frontend/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
+        res.sendFile(path.join(__dirname + '..', 'frontend/build/index.html'));
     });
 }
 
